db904ef0bd0d52360d05c8a3ec92138b4c41c4ad
reeperx <acebany080@gmail.com>
2024-02-06 08:40:52 -0800

    python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,5 @@
 """This module contains the definition of the class Square"""
 
 class Square:
-    """This is an empty class that defines a square"""
+    """This is an empty class that defines a square."""
     pass
