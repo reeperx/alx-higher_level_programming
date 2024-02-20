c98d7dfe2d68b658a2b31bba47ad362b8531b0f7
reeperx <acebany080@gmail.com>
2024-02-20 04:33:40 -0800

    chnages

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,24 +3,25 @@
 Contains the clas "Student"
 """
 
-
 class Student:
-    """Representation of a student"""
+    """
+    Defines a Student by first name, last name and age
+    """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """
+        Instantiates a Student with the given first name, last name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
+        """
+        Retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
         if attrs is None:
             return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        return {attr: getattr(self, attr) for attr in attrs if attr in self.__dict__}
