555a0bc438c744ae0cda898e4791ce57b80645d7
reeperx <acebany080@gmail.com>
2024-02-20 12:17:14 +0000

    python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines a class Student."""
 
 
@@ -7,7 +8,6 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -19,14 +19,12 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-
         If attrs is a list of strings, represents only those attributes
         included in the list.
-
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+                all(type(element) == str for element in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
