6b9ed1a6fb7ab7d8c3889e09784c7dd9a5a266b8
reeperx <acebany080@gmail.com>
2024-02-20 02:13:49 +0000

    changes

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a class-checking function."""
 
-
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
 """return true if obj is the exact class a_class, otherwise false"""
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
