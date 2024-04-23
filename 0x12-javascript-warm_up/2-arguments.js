9c02fcdf6039eca1034146c20c99ae6cbf40106f
reeperx <acebany080@gmail.com>
2024-04-22 19:17:50 -0700

    chnages

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
+const argsLength = process.argv.length;
+
+if (argsLength === 2) {
   console.log("No argument");
-} else if (process.argv.length === 3) {
+} else if (argsLength === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
