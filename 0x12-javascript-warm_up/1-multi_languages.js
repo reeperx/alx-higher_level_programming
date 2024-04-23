a40bc7db6079cccc97217cc4ab39f658bd378e46
reeperx <acebany080@gmail.com>
2024-04-22 18:55:36 -0700

    changes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,5 +4,5 @@ const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
 const thirdLine = 'JavaScript is amazing';
 console.log(firstLine);
-cosnole.log(secondLine);
+console.log(secondLine);
 console.log(thirdLine);
